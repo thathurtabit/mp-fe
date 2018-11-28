e744b3d04c73962ae76ca799b082bf14f236f24c
Stephen Fairbanks <biggste2000@hotmail.com>
2018-11-28 14:36:39 +0000

    Chore: localise Fetch request

--- src/state/store/initState.js
+++ src/state/store/initState.js
@@ -1,6 +1,6 @@
 const initState = {
   api: process.env.NODE_ENV === 'production'
-          ? 'https://moonpig-fe-fun.surge.sh/api/response.json'
+          ? 'api/response.json'
           : 'api/response.json',
   loading: true,
   response: [],
