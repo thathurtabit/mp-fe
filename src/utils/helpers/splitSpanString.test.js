5243c83c249465c2d8d701292af66f6cb8555380
Stephen Fairbanks <biggste2000@hotmail.com>
2018-11-29 18:27:48 +0000

    Fix: span split text

--- src/utils/helpers/splitSpanString.test.js
+++ src/utils/helpers/splitSpanString.test.js
@@ -1,7 +1,10 @@
-import { splitSpanString } from './splitAndSpanString';
+import { splitSpanString } from './splitSpanString';
+
+const testString = 'Test String';
+const testStringLength = testString.length;
 
 describe('Test my splitAndSpanString', () => {
   it('Should exist', () => {
-    expect(typeof splitSpanString('test')).toBe('object');
+    expect(typeof splitSpanString(testString)).toBe('object');
   });
 });
